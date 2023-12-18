a28b73b07678993b7b77c613248df1b4b85c6efe
Mockingjay-liu <57316359+Mockingjay-liu@users.noreply.github.com>
2023-12-18 23:58:05 +0800

    Update 128. 最长连续序列.py

--- 128. 最长连续序列.py
+++ 128. 最长连续序列.py
@@ -1,23 +1,24 @@
 # 23:00
 class Solution:
     def longestConsecutive(self, nums):
-        nums = list(set(nums))
-        nums = sorted(nums)
         if nums == []:
             return 0
+        nums = sorted(list(set(nums)))
+        last = nums[0]
         count = 1
         max_count = 1
-        for i in range(0, len(nums)-1):
-            if nums[i] + 1 == nums[i+1]:
+        for i in nums[1:]:
+            if last + 1 == i:
                 count += 1
-                max_count = max(count, max_count)
             else:
                 max_count = max(count, max_count)
                 count = 1
+            last = i
+        max_count = max(count, max_count)
         return max_count
 
 S = Solution()
-nums = []
+nums = [0,3,7,2,5,8,4,6,0,1]
 print(S.longestConsecutive(nums))
 print()
 
