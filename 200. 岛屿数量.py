7d24a7ddb5a8b793c8f46f28d76bc3126fcb654a
Mockingjay-liu <57316359+Mockingjay-liu@users.noreply.github.com>
2023-12-23 22:50:09 +0800

    200

--- 200. 岛屿数量.py
+++ 200. 岛屿数量.py
@@ -34,12 +34,3 @@ class Solution:
                     flag[i][j] = 1
                     update(i, j)
         return answer
-
-S = Solution()
-grid = [
-    ["1", "1", "0", "0", "0"],
-    ["1", "1", "0", "0", "0"],
-    ["0", "0", "1", "0", "0"],
-    ["0", "0", "0", "1", "1"]
-]
-print(S.numIslands(grid))
