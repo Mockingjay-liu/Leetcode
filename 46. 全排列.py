7d21084ba8e479cdb8b07c1cc8eee2014242132b
Mockingjay-liu <57316359+Mockingjay-liu@users.noreply.github.com>
2023-12-24 17:56:16 +0800

    200

---
+++ 46. 全排列.py
@@ -0,0 +1,13 @@
+class Solution:
+    def permute(self, nums):
+        if len(nums) == 1:
+            return [[nums[0]]]
+        else:
+            x = []
+            for i in self.permute(nums[:-1]):
+                for j in range(len(i)+1):
+                    x.append(i[:j]+[nums[-1]]+i[j:])
+            return x
+S = Solution()
+nums = [1]
+print(S.permute(nums))
